61c5f5dc9340ac1b9e974f9ec35ef1ad910c3165
Reginald Eli A. Deinla <aldrineg@localhost.localdomain>
2013-09-22 20:22:50 +0800

    Compose instead.

--- problem2/__init__.py
+++ problem2/__init__.py
@@ -1,11 +1,16 @@
 
-def even_fib(n):
+def fib(n):
 	a, b = 0, 1
 
 	while a < n:
-		if a % 2 == 0:
-			yield a
+		yield a
 		b = a + b
 		a = b - a
 
+
+def even_fib(n):
+	for f in fib(n):
+		if f % 2 == 0:
+			yield f
+			
 print sum(even_fib(4000000))
